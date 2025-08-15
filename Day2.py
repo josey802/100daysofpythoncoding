8806a0b5f5eb24f2265689512ba871c296720440
Joseph Ayoola <theayoolajoseph@gmail.com>
2025-08-16 00:08:27 +0100

    Delete Day2.py

--- Day2.py
+++
@@ -1,7 +0,0 @@
-print("Welcome to the tip calculator!")
-bill = float(input("What was the total bill? $"))
-tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
-bill_percent = 1 + int(tip)/100
-people = int(input("How many people to split the bill? "))
-each_person_pay = round(bill * bill_percent/people, 2)
-print (f"Each person should pay: ${each_person_pay}")
